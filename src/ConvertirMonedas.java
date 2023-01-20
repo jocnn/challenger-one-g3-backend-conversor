43d7204bfbde8f9357301942079daff6e6bbd773
jocnn <ijann_@outlook.com>
2023-01-20 19:46:30 -0300

    se creó las funciones de cálculo

--- src/ConvertirMonedas.java
+++ src/ConvertirMonedas.java
@@ -1,5 +1,11 @@
+import javax.swing.JOptionPane;
+
 public class ConvertirMonedas {
 	
+	public void ConversorPesosArgentinosAOtraMoneda(double cantidadAConvertir, double cotizacion, String moneda) {
+		double monedaDolar = cantidadAConvertir / cotizacion;
+		monedaDolar = (double) Math.round(monedaDolar * 100d) / 100;
+		JOptionPane.showMessageDialog(null, "Tienes $ " + monedaDolar + moneda);
+	}
 	
-
 }
