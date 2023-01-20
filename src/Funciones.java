20c4e99e98b85421331d1872abbdb6b9e56e5c1b
jocnn <ijann_@outlook.com>
2023-01-20 19:47:33 -0300

    se creó las opciones y se paso los parametros

--- src/Funciones.java
+++ src/Funciones.java
@@ -1,3 +1,31 @@
-public class Funciones {
+import javax.swing.JOptionPane;
 
+public class Funciones {
+	ConvertirMonedas monedas = new ConvertirMonedas();
+
+	public void ConvertirMonedas(double dato) {
+		String opcion = (JOptionPane.showInputDialog(
+			null, 
+			"Elije la moneda a la que desea convertir el monto", 
+			"Monedas", 
+			JOptionPane.PLAIN_MESSAGE,
+			null,
+			new Object[] {
+				"De peso argentino a Dólar",
+				"De peso argentino a Peso Chileno",
+				"De peso argentino a Real"
+				}, "Seleccion")).toString();
+		
+		switch (opcion) {
+			case "De peso argentino a Dólar":
+				monedas.ConversorPesosArgentinosAOtraMoneda(dato, 183.45, " Dolares");
+				break;
+			case "De peso argentino a Peso Chileno":
+				monedas.ConversorPesosArgentinosAOtraMoneda(dato, 0.23, " Pesos chilenos");
+				break;
+			case "De peso argentino a Real":
+				monedas.ConversorPesosArgentinosAOtraMoneda(dato, 35.23, " Reales");
+				break;
+		}
+	}
 }
