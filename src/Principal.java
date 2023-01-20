2dadae8388cdb53c8f53386ab879b3fba02acd29
jocnn <ijann_@outlook.com>
2023-01-20 18:10:57 -0300

    Se diseñó la vista con JOptionPane

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,16 @@
+import javax.swing.JOptionPane;
+
 public class Principal {
     public static void main(String[] args) throws Exception {
-        
+        String opciones = (JOptionPane.showInputDialog(
+                null,
+                "Seleccione una opcion de conversión",
+                "Menu",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                new Object[] {
+                        "Conversor de Moneda",
+                        "Conversor de Temperatura"
+                }, "Seleccion")).toString();
     }
 }
