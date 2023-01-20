55ddffd58a57fdde7feea529cd44ff433b51ef50
jocnn <ijann_@outlook.com>
2023-01-20 19:49:09 -0300

    se creó instancia y se llamo al método

--- src/Principal.java
+++ src/Principal.java
@@ -2,6 +2,9 @@ import javax.swing.JOptionPane;
 
 public class Principal {
     public static void main(String[] args) throws Exception {
+
+        Funciones monedas = new Funciones();
+        
         String opciones = (JOptionPane.showInputDialog(
                 null,
                 "Seleccione una opcion de conversión",
@@ -17,7 +20,9 @@ public class Principal {
             case "Conversor de Moneda":
                 String ingresarMonto = JOptionPane.showInputDialog("Ingrese la cantidad de dinero para convertir: ");
                 double montoDouble = Double.parseDouble(ingresarMonto);
-
+                
+                monedas.ConvertirMonedas(montoDouble);
+                
                 int resp = JOptionPane.showConfirmDialog(null, "¿Deseas realizar otra conversión?");
                 if (JOptionPane.OK_OPTION == resp) {
                     System.out.println("Selecciono opción afirmativa");
